e89e37cf690019c4c16eee93992deb224d92b64e
farnooshabz <farabrishamkar@gmail.com>
2021-03-23 16:25:14 -0700

    A simple front end react project getting vidoes from an youtube API and rendering them on the page

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,18 @@
-var VideoList = () => (
+/* START SOLUTION */
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = ({videos, handleVideoListEntryTitleClick}) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {videos.map((video) =>
+      <VideoListEntry
+        key={video.etag}
+        video={video}
+        handleVideoListEntryTitleClick={handleVideoListEntryTitleClick}
+      />
+    )}
   </div>
 );
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
@@ -14,6 +20,4 @@ VideoList.propTypes = {
   videos: PropTypes.array.isRequired
 };
 
-// In the ES6 spec, files are "modules" and do not share a top-level scope.
-// `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
